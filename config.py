14d617ea09570757825d9bf3fcd94df5b829ce40
sumit10869 <142525697+sumit10869@users.noreply.github.com>
2024-05-18 09:21:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 class Config:
-    API_ID = os.environ.get("API_ID", "11978076")
-    API_HASH = os.environ.get("API_HASH", "bc7a1ff026c4305bcf4f38807aabbec5")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5276059751:AAFCja69AW-EVshgRkqblUXRI95sblvIOTQ") 
+    API_ID = os.environ.get("API_ID", "21513517")
+    API_HASH = os.environ.get("API_HASH", "838d3451485b95722878921877f12066")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7123612475:AAH5T6k6W253HuK2wb8kHPQtui4NQn-XcD8") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "mongodb+srv://sckd88645:sckd88645@cluster0.gtlmd2b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://sumit333:sumit333@cluster0.emqf08v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DB_NAME = os.environ.get("DB_NAME", "cluster0")
-    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '5264572437').split()]
+    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '5904348755').split()]
 
 
 class temp(object): 
